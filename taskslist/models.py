56ed78099aa613d268af4aacbe12ebc8cd4feb1a
IvanGLS <gladiolus0707@gmail.com>
2023-01-04 12:44:33 +0200

    Add models

--- taskslist/models.py
+++ taskslist/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=255)
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    deadline = models.DateTimeField(null=True)
+    tag = models.ManyToManyField(Tag,
+                                 related_name="tag", )
+
+    class Meta:
+        ordering = ["-created_date"]
+
+    def __str__(self):
+        return "{}".format(self.content)
