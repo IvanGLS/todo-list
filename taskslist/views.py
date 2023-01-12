9b49ec5755bf585539dd44b5b63ef0d226866025
IvanGLS <gladiolus0707@gmail.com>
2023-01-12 12:33:23 +0200

    Update urls, views

--- taskslist/views.py
+++ taskslist/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+class TasksListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    ordering = ["-created_date"]
+    paginate_by = 6
+    template_name = "taskslist/index.html"
+    queryset = Task.objects.all()
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:tasks-list")
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:tasks-list")
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:tasks-list")
