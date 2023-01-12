14a0c07a91c3ee1402850dcac2a57c72cad93207
IvanGLS <gladiolus0707@gmail.com>
2023-01-12 12:31:09 +0200

    Add base.html, forms. Update models

---
+++ taskslist/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from taskslist.models import Task, Tag
+
+
+class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    deadline = forms.DateTimeField(
+        input_formats=['%d/%m/%Y %H:%M:%S'],
+        widget=forms.DateTimeInput(format='%d/%m/%Y %H:%M:%S')
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
--- taskslist/models.py
+++ taskslist/models.py
@@ -4,16 +4,24 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
+
+class User(AbstractUser):
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
 class Tag(models.Model):
     tag_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.tag_name
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
     created_date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
     deadline = models.DateTimeField(null=True)
-    tag = models.ManyToManyField(Tag,
-                                 related_name="tag", )
+    tags = models.ManyToManyField(Tag,
+                                 related_name="tasks", )
 
     class Meta:
         ordering = ["-created_date"]
